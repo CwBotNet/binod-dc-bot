f6f0b92419fbfbbe3a73a70556976a181d798fc5
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-05-06 21:32:32 +0530

    added some intents

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, IntentsBitField } from "discord.js";
 
 import dotenv from "dotenv";
 
@@ -6,9 +6,10 @@ dotenv.config();
 
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
   ],
 });
 
@@ -18,14 +19,32 @@ client.on("ready", () => {
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
-
-  message.reply({
-    content: `hye from binod bot`,
-  });
+  if (message.content.toLowerCase().includes("hello")) {
+    message.reply({
+      content: `hye from binod bot`,
+    });
+  }
 });
 
 client.on("interactionCreate", async (interaction) => {
-  await interaction.reply(`pong @${interaction.user.username}`);
+  await interaction.reply(`pong @${interaction.user.tag}`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  // console.log(interaction);
+  if (interaction.commandName === "help") {
+    await interaction.reply(`List of my commands:
+    /help - show all commad list
+    /play - play song
+    /pause - pause the song
+    /stop - stop the song
+    /addtoque - add song to running que
+    /addtolist - add song to list
+    /exit - exit the for server
+    `);
+  }
 });
 
 client.login(process.env.TOKEN);
